77e3025292b7b311fa6888c82b33d852ff8a7a3d
Nguyen <nguyennguyen121198@gmail.com>
2021-04-02 15:53:43 +0700

    feat(HomeViewController): create sort future
    sort for all photos A -> Z
    sort for all photos Z -> A
    sort for every section (when filters are chosen)

--- W3-Project1-Gumi/UI/HomeView/HomeViewController.swift
+++ W3-Project1-Gumi/UI/HomeView/HomeViewController.swift
@@ -19,7 +19,35 @@ class HomeViewController: UIViewController {
     var listOfTagFilter = [Int]()
     var listOfAllPhoto = [ Photo(url: "https://picsum.photos/id/0/5616/3744", title: "Alejandro Escamilla", tag: Tag.ART),
                            Photo(url: "https://picsum.photos/id/1/5616/3744", title: "Alejandro Escamilla", tag: Tag.NATURAL),
-                           Photo(url: "https://picsum.photos/id/100/2500/1656", title: "Tina Rataj", tag: Tag.ART)]
+                           Photo(url: "https://picsum.photos/id/100/2500/1656", title: "Tina Rataj", tag: Tag.ART),
+                           Photo(url: "https://picsum.photos/id/100/2500/1656", title: "Tina Rataj", tag: Tag.ART),
+                           Photo(url: "https://picsum.photos/id/1000/5626/3635", title: "Lukas Budimaier", tag: Tag.FOOD),
+                           Photo(url: "https://picsum.photos/id/1001/5616/3744", title: "Danielle MacInnes", tag: Tag.FASHION),
+                           Photo(url: "https://picsum.photos/id/1002/4312/2868", title: "NASA", tag: Tag.UNDEFINED),
+                           Photo(url: "https://picsum.photos/id/1003/1181/1772", title: "E+N Photographies", tag: Tag.ANIMAL),
+                           Photo(url: "https://picsum.photos/id/1004/5616/3744", title: "Greg Rakozy", tag: Tag.NATURAL),
+                           Photo(url: "https://picsum.photos/id/1005/5760/3840", title: "Matthew Wiebe", tag: Tag.FOOD),
+                           Photo(url: "https://picsum.photos/id/1006/3000/2000", title: "Vladimir Kudinov", tag: Tag.FASHION),
+                           Photo(url: "https://picsum.photos/id/1008/5616/3744", title: "Benjamin Combs", tag: Tag.ACTIVE),
+                           Photo(url: "https://picsum.photos/id/1009/5000/7502", title: "Christopher Campbell", tag: Tag.GAME),
+                           Photo(url: "https://picsum.photos/id/101/2621/1747", title: "Christian Bardenhorst", tag: Tag.ANIMAL),
+                           Photo(url: "https://picsum.photos/id/1010/5184/3456", title: "Samantha Sophia", tag: Tag.UNDEFINED),
+                           Photo(url: "https://picsum.photos/id/1011/5472/3648", title: "Roberto Nickson", tag: Tag.PARTY),
+                           Photo(url: "https://picsum.photos/id/1012/3973/2639", title: "Scott Webb", tag: Tag.PERSON),
+                           Photo(url: "https://picsum.photos/id/1013/4256/2832", title: "Cayton Heath", tag: Tag.TECHNICAL),
+                           Photo(url: "https://picsum.photos/id/1014/6016/4000", title: "Oscar Keys", tag: Tag.UNDEFINED),
+                           Photo(url: "https://picsum.photos/id/1015/6000/4000", title: "Alexey Topolyanskiy", tag: Tag.PERSON),
+                           Photo(url: "https://picsum.photos/id/1016/3844/2563", title: "Philippe Wuyts", tag: Tag.NATURAL),
+                           Photo(url: "https://picsum.photos/id/1018/3914/2935", title: "Andrew Ridley", tag: Tag.ACTIVE),
+                           Photo(url: "https://picsum.photos/id/1019/5472/3648", title: "Patrick Fore", tag: Tag.FOOD),
+                           Photo(url: "https://picsum.photos/id/102/4320/3240", title: "Ben Moore", tag: Tag.GAME),
+                           Photo(url: "https://picsum.photos/id/1020/4288/2848", title: "Adam Willoughby-Knox", tag: Tag.ACTIVE),
+                           Photo(url: "https://picsum.photos/id/1021/2048/1206", title: "Frances Gunn", tag: Tag.UNDEFINED),
+                           Photo(url: "https://picsum.photos/id/1022/6000/3376", title: "Vashishtha Jogi", tag: Tag.ART),
+                           Photo(url: "https://picsum.photos/id/1023/3955/2094", title: "William Hook", tag: Tag.FASHION),
+                           Photo(url: "https://picsum.photos/id/1024/1920/1280", title: "ÐœÐ°Ñ€Ñ‚Ð¸Ð½ Ð¢Ð°ÑÐµÐ²", tag: Tag.NATURAL),
+                           Photo(url: "https://picsum.photos/id/1025/4951/3301", title: "Matthew Wiebe", tag: Tag.PERSON)]
+    
     var listOfPhoto = [Photo]()
     
     override func viewDidLoad() {
@@ -34,7 +62,7 @@ class HomeViewController: UIViewController {
         tagsCollectionView.register(SectionHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader")
         tagsCollectionView.delegate = self
         tagsCollectionView.dataSource = self
-        tagsCollectionView.allowsMultipleSelection = true
+        tagsCollectionView.allowsMultipleSelection = false
         
         photosCollectionView.register(PhotoCollectionViewCell.nib, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         photosCollectionView.register(SectionHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader")
@@ -58,6 +86,26 @@ class HomeViewController: UIViewController {
         }
         return counter
     }
+    
+    func sortPhotoCollection(option: Int) { //  0: A → Z    |   1: Z → A
+        switch option {
+        case 0 where listOfTagFilter.isEmpty:
+            print("~~~~~~~~~~~~~~~~~~~~~~~ Sort A → Z for all")
+            listOfPhoto = listOfPhoto.sorted{ $0.title < $1.title }
+        case 0 where !listOfTagFilter.isEmpty:
+            print("~~~~~~~~~~~~~~~~~~~~~~~ Sort A → Z for filters")
+            listOfPhoto = listOfPhoto.sorted{ $0.tag == $1.tag && $0.title < $1.title }
+        case 1 where listOfTagFilter.isEmpty:
+            print("~~~~~~~~~~~~~~~~~~~~~~~ Sort Z → A for all")
+            listOfPhoto = listOfPhoto.sorted{ $0.title > $1.title }
+        default:
+            print("~~~~~~~~~~~~~~~~~~~~~~~ Sort Z → A for filters")
+            listOfPhoto = listOfPhoto.sorted{ $0.tag == $1.tag && $0.title > $1.title }
+        }
+        
+        photosCollectionView.reloadData()
+    }
+    
 }
 
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -107,6 +155,8 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TagCollectionViewCell.identifier, for: indexPath) as! TagCollectionViewCell
             if indexPath.row < 2 {
                 cell.bindData(tag: listOfSortFilter[indexPath.row])
+                cell.isSelected = false
+                cell.toggleSelected()
             } else {
                 cell.isSelected = true
                 cell.toggleSelected()
@@ -150,6 +200,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         if collectionView == tagsCollectionView {
             if let cell = collectionView.cellForItem(at: indexPath) as? TagCollectionViewCell {
                 print("+++++++++++++++++++++++ \(indexPath)")
+                sortPhotoCollection(option: indexPath.row)
                 cell.toggleSelected()
             }
         }
